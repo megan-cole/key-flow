4b1dcdfc3d1e9215b0092f33f5d171b1842474ab
Kaitlyn <kek.krause03@gmail.com>
2024-09-14 20:13:28 -0400

    fixed home url

--- keyflow/keyflow/settings.py
+++ keyflow/keyflow/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
     'typetest.apps.TypetestConfig',   #typetest/apps.py
     'users.apps.UsersConfig',         #users/app.py
 ]
