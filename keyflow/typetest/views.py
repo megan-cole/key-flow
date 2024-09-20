0c27fd41434c3f50699ef7ff346526ee039f9069
Kaitlyn <kek.krause03@gmail.com>
2024-09-19 21:23:17 -0400

    added error checking in registration fixes #18

--- keyflow/typetest/views.py
+++ keyflow/typetest/views.py
@@ -10,17 +10,40 @@ def typetest(request):
     return render(request, 'typetest.html')
 
 def register_view(request):
+    error =""
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             try:
                 data = form.cleaned_data
-                newuser = Accounts.objects.create(username=data['username'], emailAddress=data['emailAddress'], firstName=data['firstName'], lastName=data['lastName'], password=['password2'], battlePass=data['battlePass'])
-                newuser.save()
-                return redirect("/")
+
+                #check for duplicate username
+                usern = Accounts.objects.filter(username = data['username'].lower())
+                if usern.count():
+                    error = "Username already exists"
+                    form = UserRegistrationForm()
+                    return render(request, "register.html", {"form": form, "error": error})
+
+                #check for duplicate emails
+                email = Accounts.objects.filter(emailAddress = data['emailAddress'].lower())
+                if email.count():
+                    error = "Email already registered"
+                    form = UserRegistrationForm()
+                    return render(request, "register.html", {"form": form, "error": error})
+
+                #check if passwords match
+                if data['password1'] != data['password2']:
+                    error = "Passwords don't match"
+                    form = UserRegistrationForm()
+                    return render(request, "register.html", {"form": form, "error": error})
+                
+                #add user to database
+                if error == "":
+                    newuser = Accounts.objects.create(username=data['username'], emailAddress=data['emailAddress'], firstName=data['firstName'], lastName=data['lastName'], password=data['password2'], battlePass=data['battlePass'])
+                    newuser.save()
+                    return redirect("/")
             except Exception as e:
                 print(e)
-            #finally:
                 
     form = UserRegistrationForm()
-    return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form, "error": error})
